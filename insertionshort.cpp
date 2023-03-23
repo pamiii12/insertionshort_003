b81100d6a16c7baa37293b59ae5b3c3661bb6e01
pamiii12 <fahmiadityaa03@gmail.com>
2023-03-23 10:25:05 +0700

    Line 7 - 21

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -4,9 +4,18 @@ using namespace std;
 int arr[20];   // Membuat array dengan panjang data 20
 int n;         // Membuat variable inmputan n
 
-void input() {
-	while (true)
+void input() {       //Procedure Input
+	while (true)     
 	{
-
+		cout << "Masukkan Jumlah Data pada Array : ";        // Membuat inputan jumlah Element Array
+		cin >> n;                                            // Memanggil variable inputan n
+		
+		if (n <= 20) {                           // Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";   // Menampilkan pesan jika data lebih ari 20
+		}
 	}
 }
