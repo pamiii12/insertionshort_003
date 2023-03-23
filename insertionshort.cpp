540e0dc743cc8370b535407ddbc05dddeed8d79e
pamiii12 <fahmiadityaa03@gmail.com>
2023-03-23 11:22:14 +0700

    Line void display & int main 61 - 77

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -55,6 +55,24 @@ void insertionsort() {                         // Procedure Insertionsort
 			cout << arr[k] << " ";          // Output ke layar
 		}
 
+	}		
+}
+
+void display() {     // Procedure display
+	cout << endl;      //Output baris kosong
+	cout << "\n===============================" << endl;      // Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;      // Output ke layar
+	cout << "\n===============================" << endl;      // Output ke layar
+
+	for (int j = 0; j < n; j++) {     // Looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;       // Output ke layar
 	}
-		
+	cout << endl;    // Output baris kosong
+}
+
+int main() 
+{ 
+	input();              // Memanggil input
+	insertionsort();      // Memanggil insertionsort
+	display();            // Memanggil display
 }
