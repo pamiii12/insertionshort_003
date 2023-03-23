190cd98b8cc9fe3d84d2d27588c63e9b87ac2a7c
pamiii12 <fahmiadityaa03@gmail.com>
2023-03-23 10:45:17 +0700

    Line 26 - 31

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -5,11 +5,11 @@ int arr[20];   // Membuat array dengan panjang data 20
 int n;         // Membuat variable inmputan n
 
 void input() {       //Procedure Input
-	while (true)     
+	while (true)
 	{
 		cout << "Masukkan Jumlah Data pada Array : ";        // Membuat inputan jumlah Element Array
 		cin >> n;                                            // Memanggil variable inputan n
-		
+
 		if (n <= 20) {                           // Membuat kondisi n tidak lebih dari 20
 			break;
 		}
@@ -18,4 +18,16 @@ void input() {       //Procedure Input
 			cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";   // Menampilkan pesan jika data lebih ari 20
 		}
 	}
+	cout << endl;                                    // Membuat jarak per baris program
+	cout << "======================" << endl;        // Membuat tampilan susunan data element pada array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)   // Menggunakan perulangan for untuk menyimpan data array
+	{
+
+	}
+
+
+
 }
