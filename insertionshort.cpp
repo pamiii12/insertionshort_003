1851bd80e5a526caf8b7d76e277bf19baaf681a6
pamiii12 <fahmiadityaa03@gmail.com>
2023-03-23 10:18:14 +0700

    line 1 - 5

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
+int arr[20];   // Membuat array dengan panjang data 20
+int n;         // Membuat variable inmputan n
 
 void input() {
 	while (true)
